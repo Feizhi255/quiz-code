42eef862534247ae5088bb0dbbd88304e0064fdf
Jordan Kirby <jrock255@hotmail.com>
2020-08-15 21:56:17 -0600

    worked on finaly adding questions and loop them through the index

--- js.js
+++ js.js
@@ -1,189 +1,90 @@
-//on click count down timer
-var timeEl = document.querySelector(".countDown");
-var mainEl = document.getElementById("main");
-let questionArea = "";
-
-var secondsLeft = 60;
-
-document.querySelector('.start-quiz').addEventListener('click', function () {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            questionArea();
-            sendMessage();
-        }
-
-    }, 1000);
-})
-
-//time runs put
-function sendMessage() {
-    timeEl.textContent = "Time is up ";
-
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
 }
-
-//corret answer
-var correct = "";
-
-//wrong answer
-var wrong = "";
-
-//next question?
-var next = "";
-
-//store the score
-var score = "";
-
-//Notes next to work on is moving to the next question once first is ansowered
-
-
-//question starts
-document.querySelector('.start-quiz').addEventListener('click', function () {
-    var question = document.getElementById('question');
-    question.textContent += "How Are You?";
-
-    var answer = document.getElementById('answer')
-
-    //correct answer
-    var btn1 = document.getElementById('btn1')
-    btn1.textContent += "Good";
-    document.querySelector('#btn1').addEventListener('click', function(){
-        answer.textContent += "Correct!"
-        score = 1;
-    })
-
-    var btn2 = document.getElementById('btn2')
-    btn2.textContent += "Awsome";
-    document.querySelector('#btn2').addEventListener('click', function(){
-        answer.textContent += "Wrong!!"
-
-    })
-
-    var btn3 = document.getElementById('btn3')
-    btn3.textContent += "Ok";
-    document.querySelector('#btn3').addEventListener('click', function(){
-        answer.textContent += "Wrong!!!"
-
-    })
-
-    var btn4 = document.getElementById('btn4')
-    btn4.textContent += "Bad";
-    document.querySelector('#btn4').addEventListener('click', function(){
-        answer.textContent += "Wrong!!!!"
-
-    })
-})
-
-
-
-//setTime();
-
-
-
-
-
-//variables
-
-//title
-// let title = document.createElement('title')
-// console.log(title)
-// title.textContent = 'Code Quiz'
-// document.head.appendChild(title)
-
-// //h1 tag
-// let h1 = document.createElement('h1')
-// console.log(h1)
-// h1.textContent = "Code Quiz"
-// document.body.appendChild(h1)
-// h1.parentElement.style.textAlign = "center"
-
-// //start btn
-// let btn = document.createElement('BUTTON')
-// btn.classList.add('start-quiz')
-// console.log(btn)
-// btn.innerHTML = "Start Quiz"
-// document.body.appendChild(btn)
-
-
-// //timer
-// let countDownTimer = document.createElement('h2');
-// countDownTimer.classList.add('countDown');
-// console.log(countDownTimer);
-// countDownTimer.innerHTML = "60"
-// document.body.appendChild(countDownTimer)
-
-
-
-// //div question
-// let textQuestion = document.createElement('h2');
-// console.log(textQuestion)
-// //card area
-// textQuestion.classList.add('card')
-// textQuestion.innerHTML = "how r u?"
-// document.body.append(textQuestion)
-
-// //buttons1
-// let botton1 = document.createElement('BUTTON')
-// botton1.classList.add('card-body')
-// console.log(botton1)
-// botton1.innerHTML = "Good"
-// document.body.appendChild(botton1)
-
-// //buttons2
-// let botton2 = document.createElement('BUTTON')
-// botton2.classList.add('card-body')
-// console.log(botton2)
-// botton2.innerHTML = "Amazing"
-// document.body.appendChild(botton2)
-
-// //buttons3
-// let botton3 = document.createElement('BUTTON')
-// botton3.classList.add('card-body')
-// console.log(botton3)
-// botton3.innerHTML = "ok"
-// document.body.appendChild(botton3)
-
-// //buttons4
-// let botton4 = document.createElement('BUTTON')
-// botton4.classList.add('card-body')
-// console.log(botton4)
-// botton4.innerHTML = "Bad"
-// document.body.appendChild(botton4)
-
-
-
-
-
-//click show button
-
-//     //buttons1
-//     let botton1 = document.createElement('BUTTON')
-//     botton1.classList.add('card-body')
-//     console.log(botton1)
-//     botton1.innerHTML = "Good"
-//     document.body.appendChild(botton1)
-
-//     //buttons2
-//     let botton2 = document.createElement('BUTTON')
-//     botton2.classList.add('card-body')
-//     console.log(botton2)
-//     botton2.innerHTML = "Amazing"
-//     document.body.appendChild(botton2)
-
-//     //buttons3
-//     let botton3 = document.createElement('BUTTON')
-//     botton3.classList.add('card-body')
-//     console.log(botton3)
-//     botton3.innerHTML = "ok"
-//     document.body.appendChild(botton3)
-
-//     //buttons4
-//     let botton4 = document.createElement('BUTTON')
-//     botton4.classList.add('card-body')
-//     console.log(botton4)
-//     botton4.innerHTML = "Bad"
-//     document.body.appendChild(botton4)
-//
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
